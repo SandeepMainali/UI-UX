c3baaa0ce9485218f0532d949191c0c7338702e7
Sandeep mainali <amanshrestha284@gmail.com>
2022-08-23 13:38:39 +0545

    PRA

---
+++ scss/vendors/bootstrap/2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+int r=0,a=0,h=0;
+string n;
+printf("Please Enter Employee name:");
+scanf("&n",n);
+printf("Please Enter Employee rating:");
+scanf("&r",r);
+printf("Please Enter Employee attendence:");
+scanf("&a",a);
+printf("Please Enter Employee hygeince:");
+scanf("&h",h);
+getch();
+}
